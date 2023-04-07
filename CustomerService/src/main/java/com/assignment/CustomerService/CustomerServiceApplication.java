79ee6e894ee35ded3e32a3f457dc89ccdc88a839
Nada <ouji.nada@gmail.com>
2023-04-07 09:14:13 +0200

    enable service discovery with eureka for account service & transaction service

--- CustomerService/src/main/java/com/assignment/CustomerService/CustomerServiceApplication.java
+++ CustomerService/src/main/java/com/assignment/CustomerService/CustomerServiceApplication.java
@@ -2,11 +2,14 @@ package com.assignment.CustomerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
