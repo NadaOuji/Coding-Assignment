954e7a905221c865c43fb6fc0a157c80995cfc8b
Nada <ouji.nada@gmail.com>
2023-04-07 06:10:47 +0200

    add exception handler & API error & notFoundException & TU

---
+++ CustomerService/src/main/java/com/assignment/CustomerService/exception/ApiError.java
@@ -0,0 +1,34 @@
+package com.assignment.CustomerService.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class ApiError {
+
+    private final HttpStatus status;
+    private final String message;
+    private final List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message) {
+        this(status, message, null);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
