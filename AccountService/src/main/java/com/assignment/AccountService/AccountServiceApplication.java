1a37fc0179c84dc0acf61337eac001003604d0ea
Nada <ouji.nada@gmail.com>
2023-04-11 06:25:59 +0200

    bug fixing discovery Server

--- AccountService/src/main/java/com/assignment/AccountService/AccountServiceApplication.java
+++ AccountService/src/main/java/com/assignment/AccountService/AccountServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +17,16 @@ public class AccountServiceApplication {
 
         SpringApplication.run(AccountServiceApplication.class, args);
     }
+/**
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+            .build();
+    }**/
 
 
 }
