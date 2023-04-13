66c558f773dfcf9b68ca0bf238fb544b5baa0971
Nada <ouji.nada@gmail.com>
2023-04-13 16:26:16 +0200

    add log4j2 to Transaction Service and log Transaction API & errors

--- TransactionService/src/main/java/com/assignment/TransactionService/exception/TransactionNotFoundException.java
+++ TransactionService/src/main/java/com/assignment/TransactionService/exception/TransactionNotFoundException.java
@@ -1,11 +1,17 @@
 package com.assignment.TransactionService.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TransactionNotFoundException extends RuntimeException {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionNotFoundException.class);
+
     public TransactionNotFoundException(String message) {
         super(message);
+        logger.error("Transaction not found: {}", message);
+
     }
 }
